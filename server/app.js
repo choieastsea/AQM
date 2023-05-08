dc656e8dde1b4ce10c90ee9bbdd737b62acd24df
gurwls0122 <80228712+gurwls0122@users.noreply.github.com>
2023-05-09 04:17:33 +0900

    Feat : 서버 추가

---
+++ server/app.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const axios = require('axios')
+const { MqttClient } = require('mqtt')
+const app = express()
+
+
+const port = 3000
+const serviceKey = "9OdEbSYuSWsY3x8Jk%2Bm%2FbFKeOKKPfY6olRpGAUQ8QVVC3xgfbEq8NdZvwscyJTv0KpH2TJIX3E3YylLo%2BUntsA%3D%3D"
+const interval = 100000
+
+function callAPI(){
+    var url = 'http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty';
+    var queryParams = '?' + encodeURIComponent('serviceKey') + '=' + serviceKey;
+    queryParams += '&' + encodeURIComponent('returnType') + '=' + encodeURIComponent('json'); /* */
+    queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('100'); /* */
+    queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /* */
+    queryParams += '&' + encodeURIComponent('stationName') + '=' + encodeURIComponent('광진구'); /* */
+    queryParams += '&' + encodeURIComponent('dataTerm') + '=' + encodeURIComponent('DAILY'); /* */
+    queryParams += '&' + encodeURIComponent('ver') + '=' + encodeURIComponent('1.0'); /* */
+ 
+    axios.get(url + queryParams)
+    .then(response =>{
+        recent_data = response.data.response.body.items[0]
+        console.log(recent_data)
+        pm10 = recent_data["pm10Value"]
+        pm25 = recent_data["pm25Value"]
+        console.log(`pm10 : ${pm10}, pm25 : ${pm25}`)
+    })
+    .catch(error =>{
+        console.log(error)
+    });
+}
+
+app.get('/', (req, res)=>{
+    callAPI()
+    res.send("Hello World")
+})
+
+setInterval(()=>{
+    callAPI()
+}, 500000)
+
+app.listen(port, ()=>{
+    console.log(`Listening on port : ${port}`)
+})
