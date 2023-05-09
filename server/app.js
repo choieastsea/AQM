85e8a964704e77dc4b3cfd49c0713793415faa28
gurwls0122 <gurwls0122@naver.com>
2023-05-09 12:30:22 +0000

    REST API

--- server/app.js
+++ server/app.js
@@ -10,8 +10,9 @@ const db = mongoose.connection;
 const port = 3000
 const serviceKey = "9OdEbSYuSWsY3x8Jk%2Bm%2FbFKeOKKPfY6olRpGAUQ8QVVC3xgfbEq8NdZvwscyJTv0KpH2TJIX3E3YylLo%2BUntsA%3D%3D"
 const interval = 100000
+const moment = require('moment-timezone');
 
-function callAPI(){
+function callAPI() {
     var url = 'http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty';
     var queryParams = '?' + encodeURIComponent('serviceKey') + '=' + serviceKey;
     queryParams += '&' + encodeURIComponent('returnType') + '=' + encodeURIComponent('json'); /* */
@@ -20,35 +21,79 @@ function callAPI(){
     queryParams += '&' + encodeURIComponent('stationName') + '=' + encodeURIComponent('광진구'); /* */
     queryParams += '&' + encodeURIComponent('dataTerm') + '=' + encodeURIComponent('DAILY'); /* */
     queryParams += '&' + encodeURIComponent('ver') + '=' + encodeURIComponent('1.0'); /* */
- 
+
     axios.get(url + queryParams)
-    .then(response =>{
-        recent_data = response.data.response.body.items[0]
-        console.log(recent_data)
-        pm10 = recent_data["pm10Value"]
-        pm25 = recent_data["pm25Value"]
-        console.log(`pm10 : ${pm10}, pm25 : ${pm25}`)
-        data = JSON.parse(`{"pm10" : "${pm10}", "pm25" : "${pm25}"}`)
-            db.collection('SCollection').insertOne(data, function(err, result) {
-            if (err) throw err;
-            console.log('Saved to MongoDB:', data);
-          });
-    })
-    .catch(error =>{
-        console.log(error)
-    });
+        .then(response => {
+            recent_data = response.data.response.body.items[0]
+            pm10 = recent_data["pm10Value"]
+            pm25 = recent_data["pm25Value"]
+
+            currentTime = new Date();
+            console.log(`pm10 : ${pm10}, pm25 : ${pm25}, "time" : "${currentTime}"`)
+            data = JSON.parse(`{"pm10" : "${pm10}", "pm25" : "${pm25}", "time" : "${currentTime}"}`)
+            db.collection('SCollection').insertOne(data, function (err, result) {
+                if (err) throw err;
+                console.log('Saved to MongoDB:', data);
+            });
+        })
+        .catch(error => {
+            console.log(error)
+        });
 }
 
-app.get('/', (req, res)=>{
-    callAPI()
-    res.send("Hello World")
+const getOuterPM = async () => {
+    const currentTime = new Date();
+    const apiData = [];
+
+    const res = await db.collection('SCollection').find({}).toArray();
+    for (var i = 0; i < res.length; i++) {
+        const time = new Date(res[i].time);
+        if (currentTime - time < 2 * 60 * 60 * 1000) {
+            apiData.push(res[i]);
+        }
+    }
+    console.log(apiData);
+    return apiData;
+}
+
+const getInnerPM = async() =>{
+    const currentTime = new Date();
+    const sensorData = [];
+    currentTime.setHours(currentTime.getHours()+9)
+
+    const res = await db.collection('AQCollection').find({}).toArray();
+    //10초마다 저장될때 5분짜리 데이터를 보내주기 위해서
+    var count = 0;
+    for (var i = 0 ; i<res.length; i++){
+        const time = new Date(res[i].time);
+        if (currentTime - time <2*60*60*1000){
+            if (count==0){
+                sensorData.push(res[i]);
+                count+=1
+            }
+            else{
+                count+=1
+                if (count%30 == 0){
+                    count = 0
+                }
+            }
+        }
+    }
+    console.log(sensorData);
+    return sensorData;
+}
+
+app.get('/getpm', async(req, res) => {
+    outerData = await getOuterPM()
+    innerData = await getInnerPM()
+    res.send({innerData, outerData})
 })
 
-setInterval(()=>{
+setInterval(() => {
     callAPI()
-}, 500000)
+}, 300000)
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Listening on port : ${port}`)
     //mongodb 연결
     mongoose.connect(MONGO_DB_IP);
--- server/mqtt.js
+++ server/mqtt.js
@@ -32,7 +32,7 @@ client.on('message', (topic, message) => {
     // 미세먼지 데이터 parsing
     const AQData = JSON.parse(message.toString());
     //AQData : { pm10: 5.7, pm25: 3.2, time: '08.05.2023 16:37:17' }
-    db.collection('AQCollection').insertOne(AQData, function(err, result) {
+    db.collection('AQCollection').insertOne(AQData, function (err, result) {
       if (err) throw err;
       console.log('Saved to MongoDB:', AQData);
     });
@@ -46,47 +46,52 @@ client.on('message', (topic, message) => {
 async function getData() {
   const aq_collection = db.collection('AQCollection');
   const seooul_collection = db.collection('SCollection');
-  
+
   // 특정 조건을 만족하는 데이터를 가져옴
   const aq_data = await aq_collection.findOne({}, { sort: { _id: -1 } });
   const s_data = await seooul_collection.findOne({}, { sort: { _id: -1 } });
-  
-  const ret = []
+
+  const ret = [];
   // 외부에 있고, 외부 미세먼지가 좋지 않으면 마스크 착용 권고
   //
 
   // 실내에 있고, 외부 미세먼지가 좋지 않으면 외출 자제 권고
-  if(s_data.pm10 > 81|| s_data.pm25 > 36) {
-    console.log('현재 외부 미세먼지가 나쁨 수준입니다. 외출시 마스크를 챙기세요')
+  if (s_data.pm10 > 81 || s_data.pm25 > 36) {
+    console.log('현재 외부 미세먼지가 나쁨 수준입니다. 외출시 마스크를 챙기세요');
     ret.push('현재 외부 미세먼지가 나쁨 수준입니다. 외출시 마스크를 챙기세요');
   }
 
   // 실내의 미세먼지가 좋지 않을 때 공기청정기 작동 권고
-  if(aq_data.pm10 > 81|| aq_data.pm25 > 36) {
-    console.log('공기청정이 필요합니다.')
+  if (aq_data.pm10 > 81 || aq_data.pm25 > 36) {
+    console.log('공기청정이 필요합니다.');
     ret.push('공기청정이 필요합니다.');
   }
 
   // 실내에 있고, 실내 미세먼지가 외부 미세먼지보다 좋지 않으면 환기 권고
-  if(s_data.pm10 < aq_data.pm10 || s_data.pm25 < aq_data.pm25){
-    console.log('환기 시키세요')
+  if (s_data.pm10 < aq_data.pm10 || s_data.pm25 < aq_data.pm25) {
+    console.log('환기 시키세요');
     ret.push('환기 시키세요');
   }
   // MongoDB 연결 종료
   // await client.close();
-  
+
   return ret;
+}
+async function getRecentOutdoorData() {
+  const seooul_collection = db.collection('SCollection');
+  const seoul_data = await seooul_collection.findOne({}, { sort: { _id: -1 } });
+  return seoul_data;
 }
 
 // MQTT 클라이언트에게 데이터 전송
-function sendData(data) {
+function sendData(topic, data) {
   const client = mqtt.connect(MQTT_BROKER_IP);
-  
-  client.on('connect', function() {
+
+  client.on('connect', function () {
     // 데이터를 JSON 형식으로 변환하여 클라이언트에게 전송
-    client.publish('alert/airout', JSON.stringify(data), function() {
+    client.publish(topic, JSON.stringify(data), function () {
       console.log('Data published');
-      
+
       // MQTT 연결 종료
       // client.end();
     });
@@ -94,17 +99,28 @@ function sendData(data) {
 }
 
 // 5분마다 MongoDB에서 데이터 가져오기
-setInterval(async function() {
+setInterval(async function () {
   console.log('Checking for new data');
-  
+
   const data = await getData();
-  
+
   if (data) {
     console.log('Alert to client:', data);
-    
+
     // 데이터가 있으면 MQTT 클라이언트에게 전송
-    sendData(data);
+    sendData('alert/airout', data);
   } else {
     console.log('No new data');
   }
 }, 5 * 60 * 1000);
+
+// 5분마다 외부 공기질 데이터 보내기
+setInterval(async function () {
+  const data = await getRecentOutdoorData();
+  if (data) {
+    sendData('/outdoor', data);
+    console.log('outdoor to client:', data);
+  } else {
+    console.log('No new data');
+  }
+}, 5 * 1000);
